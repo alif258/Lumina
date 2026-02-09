669c331f90ce25bd1231748120f63009cd68d1ab
Alif Hossain <alifhossain20050208@gmail.com>
2026-02-09 17:57:40 +0600

    fix: image case sensitivity

--- src/components/Bags.jsx
+++ src/components/Bags.jsx
@@ -4,7 +4,7 @@ const Bags = () => {
   return (
     <div className="px-6 md:px-10 lg:px-0">
   <div
-  className="bg-[url('./assets/Mblbags.png')] xl:bg-[url('./assets/bag.png')] bg-no-repeat bg-cover xl:bg-contain bg-center xl:bg-bottom max-w-355 m-auto h-140 md:h-[600px] lg:h-140 xl:h-90.5 flex flex-col justify-start xl:justify-end items-center xl:items-start pt-10 xl:pt-0 md:pb-8 lg:pb-10 xl:pl-8 px-8 text-center xl:text-left rounded-3xl overflow-hidden"
+  className="bg-[url('./assets/mblbag.png')] xl:bg-[url('./assets/bag.png')] bg-no-repeat bg-cover xl:bg-contain bg-center xl:bg-bottom max-w-355 m-auto h-140 md:h-[600px] lg:h-140 xl:h-90.5 flex flex-col justify-start xl:justify-end items-center xl:items-start pt-10 xl:pt-0 md:pb-8 lg:pb-10 xl:pl-8 px-8 text-center xl:text-left rounded-3xl overflow-hidden"
 >
   {/* Heading */}
   <h1 className="mb-4 md:mb-4 lg:mb-5 text-[32px] md:text-[36px] lg:text-[32px] xl:text-[30px] text-[#000000] font-bold md:mt-5">
