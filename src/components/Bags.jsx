04ddc87fc771d35ee8235689be6d9b072265005e
Alif Hossain <alifhossain20050208@gmail.com>
2026-02-10 10:58:28 +0600

    Fix: login button responsive issue on mobile

--- src/components/Bags.jsx
+++ src/components/Bags.jsx
@@ -1,33 +1,35 @@
-import mblBag from "../assets/mblbag.png";
-import desktopBag from "../assets/bag.png";
+import React from 'react';
+import Portability from "../assets/Portability.webp";
 
 const Bags = () => {
   return (
-    <div className="px-6 md:px-10 lg:px-0">
-      <div
-        style={{ backgroundImage: `url(${mblBag})` }}
-        className="bg-no-repeat bg-cover bg-center max-w-355 m-auto h-[500px] md:h-[600px] lg:h-[560px] xl:h-[450px] flex flex-col justify-start xl:justify-end items-center xl:items-start pt-10 xl:pt-0 md:pb-8 lg:pb-10 xl:pl-16 px-8 text-center xl:text-left rounded-3xl overflow-hidden xl:bg-contain xl:bg-bottom"
-      >
-        <style jsx>{`
-          @media (min-width: 1280px) {
-            div {
-              background-image: url(${desktopBag}) !important;
-            }
-          }
-        `}</style>
-
-        {/* Heading */}
-        <h1 className="mb-4 md:mb-4 lg:mb-5 text-[28px] md:text-[36px] lg:text-[32px] xl:text-[30px] text-[#000000] font-bold md:mt-5 leading-tight">
-          A Lightweight Design That Moves With You <br className="hidden xl:block" />
-          Ultra-Slim Design for Effortless Portability
-        </h1>
-
-        {/* Paragraph */}
-        <p className="text-[16px] md:text-[18px] lg:text-[16px] xl:text-[20px] text-[#4E4D4D] font-semibold hidden md:block max-w-[560px]">
-          With a slim 4 mm profile and a lightweight 750 g build, 
-          Lumina 15F adds screen space without adding bulk.
-        </p>
-      </div>
+    <div>
+      <section className="max-w-355 mx-auto pt-10 md:pt-28 px-6 md:px-10 lg:px-0">
+        <div className="w-full h-full rounded-[30px] gradient-bg box-1 ">
+          <div className="flex flex-col md:flex-col lg:flex-row items-center gap-6">
+            
+            {/* Text Content */}
+            <div className="w-full lg:w-1/2 pb-0 p-8 lg:pl-12 lg:pb-12  md:py-12 text-center lg:text-left">
+              <h4 className="text-[24px] md:text-[30px] text-[#000000] mb-0 md:mb-6 font-bold leading-tight">
+                A Lightweight Design That Moves With You Ultra-Slim Design for Effortless Portability
+              </h4>
+              
+              <p className="hidden md:block lg:block text-[#4E4D4D] text-[18px] md:text-[20px] font-semibold">
+                With a slim 4 mm profile and a lightweight 750 g build, Lumina 15F adds screen space without adding bulk.
+              </p>
+            </div>
+            
+            {/* Image Container */}
+            <div className="w-full md:w-1/2 relative md:min-h-[150px] lg:min-h-[255px]  flex justify-center items-end">
+              <img 
+                src={Portability} 
+                alt="Portability design" 
+                className="w-[90%] md:w-full h-auto object-contain md:absolute md:right-10 md:bottom-0" 
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
