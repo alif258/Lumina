064443360b336ae8e76c92ffdb1440b2168c9676
Alif Hossain <alifhossain20050208@gmail.com>
2026-02-17 11:33:50 +0600

    refactor gradient animation with 4 color variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,9 +34,9 @@ const Header = () => {
 
       {/* SINGLE BUTTON (OUTSIDE HERO / BELOW BG) */}
       <div className="flex justify-center mt-6 md:mt-8 lg:mt-10 mb-8 lg:mb-15">
-        <button className="p-[2px] rounded-full bg-gradient-to-b from-[#492aea] via-[#ad7738] to-[#7638a9] transition-transform hover:scale-105 active:scale-95 shadow-xl">
-          <div className="bg-white px-8 py-3 md:px-12 lg:px-14 md:py-3 lg:py-4 rounded-full">
-            <span className="text-[16px] md:text-[20px] lg:text-[22px] font-bold text-black uppercase tracking-wide">
+        <button className="p-0.75 rounded-full gradient-bg box-7 transition-transform  shadow-xl">
+          <div className=" px-8 py-3 md:px-12 lg:px-10 md:py-3 lg:py-3 rounded-full bg-[#F0F3FE]">
+            <span className="text-[16px] md:text-[18px] lg:text-[20px] font-semibold text-black uppercase ">
               Buy Now
             </span>
           </div>
--- src/components/VisualExcellence.jsx
+++ src/components/VisualExcellence.jsx
@@ -10,11 +10,13 @@ const VisualExcellence = () => {
         delivering sharp clarity, vibrant colors and smooth performance. Perfect
         for work, creative tasks, gaming and productivity on the go.
       </p>
-      <button className="relative p-[2px] rounded-3xl bg-gradient-to-b from-[#492aea] via-[#ad7738] 54% to-[#7638a9] transition-transform hover:scale-105 active:scale-95">
-        <div className="bg-white px-7 md:px-10 py-2 md:py-3 rounded-[calc(1.5rem-2px)]">
-          <span className="text-[20px] font-semibold text-black">Buy Now</span>
-        </div>
-      </button>
+      <button className="p-0.75 rounded-full gradient-bg box-7 transition-transform  shadow-xl">
+          <div className=" px-8 py-3 md:px-12 lg:px-10 md:py-3 lg:py-3 rounded-full bg-[#F0F3FE]">
+            <span className="text-[16px] md:text-[18px] lg:text-[20px] font-semibold text-black uppercase ">
+              Buy Now
+            </span>
+          </div>
+        </button>
     </div>
     </div>
   );
